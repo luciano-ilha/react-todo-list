5935749b02f0dc1e8015f5abadc87022d1a0d737
Luciano Ilha <ilha.dev.br@gmail.com>
2021-06-30 20:46:07 -0300

    Add css modules

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -3,10 +3,7 @@ import React from "react";
 import "./CourseGoalItem.css";
 
 const CourseGoalItem = (props) => {
-  // const [deleteText, setDeleteText] = useState('');
-
   const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
     props.onDelete(props.id);
   };
 
